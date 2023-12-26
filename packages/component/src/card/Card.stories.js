1f2029b555f9bef97d73872134ad5b580d76222a
patled <github@lederer.cloud>
2023-12-26 06:54:05 +0100

    Add argTypes for Card component in Card.stories.js, p52

--- packages/component/src/card/Card.stories.js
+++ packages/component/src/card/Card.stories.js
@@ -2,14 +2,27 @@ import { CardComponent } from './Card';
 
 export default {
   title: 'Components/Card',
+  argTypes: {
+    image: { constrol: { type: 'text' } },
+    headline: { constrol: { type: 'text' } },
+    content: { constrol: { type: 'text' } },
+    link: { constrol: { type: 'text' } },
+  },
 };
 
-const PrimaryTemplate = () => `<in-card>
-  <img slot="header" src='https://images.unsplash.com/photo-1612392167062-8f76710986ba?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
-/>
-  <h4 slot="header">Food</h4>
-  <p slot="content">Food is any substance consumed to provide nutritional support for an organism. Food is usually of plant or animal origin, and contains essential nutrients, such as carbohydrates, fats, proteins, vitamins, or minerals.</p>
-  <a href="#" slot="footer">Read More</a>
+const PrimaryTemplate = ({ image, headline, content, link }) => `
+<in-card>
+<img slot="header" src="${image}"> <h4 slot="header">${headline}</h4>
+<p slot="content">${content}</p>
+<a href="#" slot="footer">${link}</a>
 </in-card>`;
 
 export const ImageCard = PrimaryTemplate.bind({});
+ImageCard.args = {
+  image:
+    'https://images.unsplash.com/photo-1612392167062-8f76710986ba?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+  headline: 'Food',
+  content:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  link: 'Read',
+};
