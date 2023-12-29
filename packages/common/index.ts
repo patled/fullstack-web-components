2a620ba96d607d3b8afd668a8af8f3f468648a75
patled <github@lederer.cloud>
2023-12-29 08:22:41 +0100

    Add Component decorator and ElementMeta type, p200

---
+++ packages/common/index.ts
@@ -0,0 +1,2 @@
+export { Component } from './src/decorator';
+export type { ElementMeta } from './src/decorator';
---
+++ packages/common/src/decorator/component.ts
@@ -0,0 +1,27 @@
+export interface ElementMeta {
+  selector?: string;
+  style?: string;
+  template?: string;
+}
+
+export function Component(meta: ElementMeta) {
+  if (!meta) {
+    console.error('Component must include ElementMeta to compile');
+  }
+  return (target: any) => {
+    if (!meta.template) {
+      meta.template = '';
+    }
+    if (!meta.style) {
+      meta.style = '';
+    }
+
+    target.prototype.elementMeta = meta;
+
+    if (meta.selector) {
+      customElements.define(meta.selector, target);
+    }
+
+    return target;
+  };
+}
